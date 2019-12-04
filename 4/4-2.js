7d07f0b1c1826c7ecd5c687095d0f1b33f3d8697
Dylan Steen <dsteen@petalmd.com>
2019-12-04 10:21:05 -0500

    Add 4-2.js

---
+++ 4/4-2.js
@@ -0,0 +1,11 @@
+const start = 240298;
+const end = 784956;
+
+const arr = Array(end - start + 1).fill().map((_, idx) => start + idx)
+
+const consecutiveMatch = (array) => array.some((char, index) => index < array.length - 1 && char === array[index + 1] && !(char === array[index + 2] || char === array[index - 1]));
+const nonDecreasing = (array) => array.map(char => +char).every((num, index) => index === 0 || num >= array[index - 1]);
+const parseArray = (array) => array.map(num => num.toString().split(''));
+
+const result = parseArray(arr).filter(parsed => consecutiveMatch(parsed) && nonDecreasing(parsed));
+console.log(result.length);
