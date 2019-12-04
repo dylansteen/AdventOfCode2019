f9fe7b60879c4c94c167625da600bfaa514da39d
dylansteen <50588786+dylansteen@users.noreply.github.com>
2019-12-04 12:38:01 -0500

    am i a code golfer now

--- 4/4-1.js
+++ 4/4-1.js
@@ -1,11 +1,10 @@
 const start = 240298;
 const end = 784956;
 
-const arr = Array(end - start + 1).fill().map((_, idx) => start + idx)
+const arr = Array(end - start + 1).fill().map((_, idx) => start + idx).map(num => num.toString().split(''));
 
 const consecutiveMatch = (array) => array.some((char, index) => index < array.length - 1 && char === array[index + 1]);
 const nonDecreasing = (array) => array.map(char => +char).every((num, index) => index === 0 || num >= array[index - 1]);
-const parseArray = (array) => array.map(num => num.toString().split(''));
 
-const result = parseArray(arr).filter(parsed => consecutiveMatch(parsed) && nonDecreasing(parsed));
+const result = arr.filter(mapped => consecutiveMatch(mapped) && nonDecreasing(mapped));
 console.log(result.length);
