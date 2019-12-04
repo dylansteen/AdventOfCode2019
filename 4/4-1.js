73de04947a00e9464da5959d06ff861d767730d2
Dylan Steen <dsteen@petalmd.com>
2019-12-04 10:11:00 -0500

    4-1 but it's cooler and shorter

--- 4/4-1.js
+++ 4/4-1.js
@@ -1,22 +1,11 @@
 const start = 240298;
 const end = 784956;
 
-let count = 0;
-for (i of range(start, end)) {
+const arr = Array(end - start + 1).fill().map((_, idx) => start + idx)
 
-  const parsedArray = i.toString().split('');
-  const consecutiveMatch = parsedArray.some((char, index) => index < parsedArray.length - 1 && char === parsedArray[index + 1]);
-  const nonDecreasing = parsedArray.map(char => +char).every((num, index) => index === 0 || num >= parsedArray[index - 1])
+const consecutiveMatch = (array) => array.some((char, index) => index < array.length - 1 && char === array[index + 1]);
+const nonDecreasing = (array) => array.map(char => +char).every((num, index) => index === 0 || num >= array[index - 1]);
+const parseArray = (array) => array.map(num => num.toString().split(''));
 
-  if (consecutiveMatch && nonDecreasing) {
-    count++;
-  }
-}
-
-console.log(count);
-
-function* range(start, end) {
-  for (let i = start; i <= end; i++) {
-    yield i;
-  }
-}
+const result = parseArray(arr).filter(parsed => consecutiveMatch(parsed) && nonDecreasing(parsed));
+console.log(result.length);
